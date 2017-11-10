99182e3330536c025c32d3003d8ac6e6498f9dd6
Stephen Wagstaff <srwagsta@gmail.com>
2017-11-09 22:13:53 -0600

    Config on email 2.0

--- geoProject/site_settings/live_settings.py
+++ geoProject/site_settings/live_settings.py
@@ -157,7 +157,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'srwagsta@gmail.com'
-EMAIL_HOST_PASSWORD = open(os.path.join(BASE_DIR, 'private/email_password.txt', 'r')).readline()
+EMAIL_HOST_PASSWORD = open(os.path.join(BASE_DIR, 'private/email_password.txt'), 'r').readline()
 
 
 
