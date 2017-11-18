1339a329936304e386b8b871e44b73e6a8cc1d2f
Stephen Wagstaff <srwagsta@gmail.com>
2017-11-17 22:56:08 -0600

    Quickly redirected trafic from /projects to the construction page.

--- projects/views.py
+++ projects/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 
 
 def show_projects(request):
-    return render(request, 'project_page.html')
+    return render(request, 'cycling_page.html') # 'project_page.html')
